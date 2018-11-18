eb87e705544fbfe922765594be7b4e7f65ddd7a4
elmayvazyan <elmira_ayvazayn@edu.aua.am>
2018-11-18 15:31:50 +0400

    This is my extra credit homework

---
+++ Sqaure.java
@@ -0,0 +1,41 @@
+public class Square {
+	
+	public static void main(String[] args) {
+		int result;
+		int num;
+		result = square(n);
+		Syste.out.println(result);
+	}
+	
+	static int square(int n) {
+		return n*n;
+	}
+}
+
+
+public class FactorialWithRec {
+	
+	public static void main(String[] args) {
+	int i;
+	long result;
+	result = factorial(i);
+	System.out.println(result);
+	
+	}
+	public static long factorial(int i) {
+		if(i==0)
+			return 1;
+		return i * factorial(i-1);
+	}
+}
+
+public class Summary {
+	public static void main(String[] args) {
+		int square(n);
+		int factorial(i);
+		
+		System.out.printlin("The square is " + sqaure(n) + " and the factorial is " + factorial(i));
+	}
+}
+
+	
